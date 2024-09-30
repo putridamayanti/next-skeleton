562069bf8efbf5bf279b8ffddb5d254364e2465a
Putri Damayanti <damayartdesign@gmail.com>
2024-09-30 22:59:52 +0700

    Update api instance

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,29 @@
 import axios from "axios";
+import {getItem} from "utils/storage";
+
+const apiUrl = process.env.API_URL;
 
 const Instance = axios.create({
-    baseURL: process.env.API_URL
+    baseURL: apiUrl,
 });
 
+Instance.interceptors.request.use(
+    async (config) => {
+        const token = await getItem('x-token');
+
+        if (token) {
+            config.headers['Authorization'] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+
 const Api = {
-    Instance
+    Instance,
 }
 
 export default Api;
